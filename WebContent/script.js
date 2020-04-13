d2094e886e7c99fd722f30d9c5b2f8eea7eb0057
chris <chris@192.168.10.101>
2020-04-13 08:04:07 -0700

    Major changes to button functionality. Aligned all html pages that are being injected in the externalPlaceholder. Added 1 img - CJ

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,22 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#Orders").click(function(){
-    $("#externalPlaceholder").load("Orders.html");
-  });
-  
-  $("#WishList").click(function(){
-    $("#externalPlaceholder").load("WishList.html");
-  });
+					$("#Orders").click(function() {
+						$("#externalPlaceholder").load("Orders.html");
+					});
 
-  $(".dropdown").hover(            
-	        function() {
-	            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-	            $(this).toggleClass('open');        
-	        },
-	        function() {
-	            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-	            $(this).toggleClass('open');       
-	        }
-	    );
-  
-});
+					$("#WishList").click(function() {
+						$("#externalPlaceholder").load("WishList.html");
+					});
+
+					$("#Profile").click(function() {
+						$("#externalPlaceholder").load("Profile.html");
+					});
+
+					$("#Logout").click(function() {
+						$("#externalPlaceholder").html("<h2 style=\"margin: auto; width: 60%;\"><br><br>You have been logged out successfully.<br><br>Thank you and enjoy your day</h2>");
+					});
+
+					$(".dropdown").hover(function() {
+						$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+						$(this).toggleClass('open');
+						},function() {
+							$('.dropdown-menu', this).not(
+										'.in .dropdown-menu').stop(true, true)
+										.slideUp("400");
+								$(this).toggleClass('open');
+							});
+
+				});
